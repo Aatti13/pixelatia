7a45af07677613101819e9ab4750f3eaea7f8acb
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-06-07 19:05:41 +0530

    Commit: Added auth documentation

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Possible Routes for Auth:
+ * Register
+ * Login
+ * Logout
+ * Get current User
+ * JWT auth
+ * OAuth
+ */
+
+
+export default router;
